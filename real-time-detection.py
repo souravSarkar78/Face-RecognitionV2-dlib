c3d190924e792ba7b6b2a2c953e22359de5b22a3
souravSarkar78 <ss830897@gmail.com>
2021-06-13 23:58:37 +0530

    Fixed some errors

--- real-time-detection.py
+++ real-time-detection.py
@@ -3,14 +3,16 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
-# from PIL import ImageGrab
- 
+
+# Define the path for training images
+
 path = 'faces'
+
 images = []
 classNames = []
-myList = os.listdir(path)
-print(myList)
-for cl in myList:
+
+# Reading the training images and classes and storing into the corresponsing lists
+for cl in os.listdir(path):
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
